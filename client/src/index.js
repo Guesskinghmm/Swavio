87af6b1bb4798bbe4087fd4d66cddda7f3dfd77e
Guesskinghmm <stayatyourpath@gmail.com>
2026-08-20 03:08:01 +0530

    Fix frontend missing auth token on API requests

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { NotificationProvider } from "./context/NotificationContext";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import axios from "axios";
+
+// Set up global axios interceptor to attach JWT token
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
